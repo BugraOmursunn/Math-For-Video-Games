4a25f432091015fde9e76254a4dd1db0082bb935
Bugra.Omursun <bugra.omursun@gmail.com>
2021-07-06 14:02:38 +0300

    Addition & Subtraction

---
+++ Math For Video Games/Assets/IntegerOverflow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntegerOverflow : MonoBehaviour {
+
+    int bigNumber = 2147483640;
+
+    // Use this for initialization
+    void Start()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(bigNumber);
+        bigNumber = bigNumber + 1;
+	}
+}
