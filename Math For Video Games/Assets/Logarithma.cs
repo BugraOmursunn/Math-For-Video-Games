be7831707ce92e765353344220bffd71c8f1b960
Bugra.Omursun <bugra.omursun@gmail.com>
2021-07-07 12:15:28 +0300

    Sync

---
+++ Math For Video Games/Assets/Logarithma.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logarithma : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Mathf.Log(32));       // base on e
+        Debug.Log(Mathf.Log(32, 2));    // base on whatever you want
+    }
+
+}
---
+++ Math For Video Games/Assets/Power.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Mathf.Pow(2, 2));
+        Debug.Log(Mathf.Pow(2, 3));
+
+        Debug.Log(Mathf.Pow(4, 0.5f));
+        Debug.Log(Mathf.Ceil(Mathf.Pow(8, 0.33f)));
+        Debug.Log(Mathf.Sqrt(4));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
